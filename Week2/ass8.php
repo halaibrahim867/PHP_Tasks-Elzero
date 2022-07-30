ace2fe285ac6c021effb1793b97fd61b496906eb
Hala Ibrahim <halaibrahim867@gmail.com>
2022-07-30 17:13:49 +0200

    end assignment 8 of week 2

---
+++ Week2/ass8.php
@@ -0,0 +1,19 @@
+<?php
+    $arr=[
+        "FrontEnd" =>[
+            "HTML", "CSS" ,"JS"=> [
+                "Vujs"=>[ "2"=> "v2" ,"v3"], "Rectjs" ,"Svelte"
+            ]
+            ], "BackEnd" =>[
+                "PHP" ,"MySQL" , "Security"
+            ],
+            "Git" , "Github" ,"Testing"=> [
+                "Unit Testing" , "End To End" , "Integration
+                "
+            ]
+            ];
+
+echo "<pre>";
+    print_r($arr);
+echo "</pre>";
+?>
