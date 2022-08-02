a5b0a3576896b1d2f9506c7c7799a507a04301fc
Hala Ibrahim <halaibrahim867@gmail.com>
2022-08-02 10:27:39 +0200

    end assignments of  week 3

---
+++ Week3/ass4.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo $_SERVER['DOCUMENT_ROOT'];
+    echo "<br>"; 
+    echo $_SERVER['SERVER_NAME'];
+    echo "<br>"; 
+    echo $_SERVER['PHP_SELF'];
+    echo "<br>";
+    //There are 2 task ,but i can't do it 
+?>
