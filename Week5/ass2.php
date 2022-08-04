349d4c0e8ab55198740511e80045dbc3f11d616e
Hala Ibrahim <halaibrahim867@gmail.com>
2022-08-04 21:27:04 +0200

    end assignment 2 of week 5

---
+++ Week5/ass2.php
@@ -0,0 +1,15 @@
+<?php
+    $a = 400;
+    $b = 200;
+    $c = 300;
+
+    if($a > $b){
+        echo "A Is Larger Than B";
+    }
+    elseif( $a > $c){
+        echo "A Is Larger Than C";
+    }
+    else{
+        echo "A Is Not Larger Than B Or C";
+    }
+?>
