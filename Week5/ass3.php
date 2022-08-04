972a07317238c48ff5eb9a51762f69efdb363aa1
Hala Ibrahim <halaibrahim867@gmail.com>
2022-08-04 21:48:09 +0200

    end assignment 3 of week 5

---
+++ Week5/ass3.php
@@ -0,0 +1,27 @@
+<?php
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+        echo "The Request Method Is Post And Username Is " .$_POST['user'];
+        echo "<br>";
+        $admins = ["Osama", "Ahmed", "Sayed"];
+        if(in_array($_POST['user'],$admins )){
+            echo "This Username ".$_POST['user'] ." Is Admin";
+        }
+    }
+   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 3 of Week 5</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="text" name="user">
+        <input type="submit" value="Send">
+    </form>
+</body>
+</html>
