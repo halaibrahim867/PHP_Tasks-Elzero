16a7bfc43ec8425ef1b1945d7011540d2ebee004
Hala Ibrahim <halaibrahim867@gmail.com>
2022-08-10 12:34:20 +0200

    end assignment 4 of week 6

---
+++ Week6/ass4.php
@@ -0,0 +1,18 @@
+<?php
+    $start = 10;
+    $end = 0;
+    $stop = 3;
+
+    for(; $start >= $stop ; $start--){
+         if($start < 10){
+            echo 0 . $start;
+            echo "<br>";
+
+        }
+        else{
+            echo $start;
+            echo "<br>";
+        }
+
+    }
+?>
