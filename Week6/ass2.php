2476e5627c43e53426658dc3ca9fb360ba871721
Hala Ibrahim <halaibrahim867@gmail.com>
2022-08-10 03:23:12 +0200

    end assignment 2 of week 6

---
+++ Week6/ass2.php
@@ -0,0 +1,24 @@
+<?php
+    $index = 0;
+    for(; $index < 20 ;){
+        echo $index += 2  ;
+        echo "<br>";
+    } 
+
+    echo "===============================" ."<br>";
+    
+    $index =0;
+    while ($index < 20){
+        echo $index += 2;
+        echo "<br>";
+    }
+
+    echo "===============================" ."<br>";
+    
+    $index =0 ;
+    do{
+        echo $index +=2 ;
+        echo "<br>";
+    }while($index< 20)
+
+?>
