cd91f33f326c93d307f3f22136d2698e9dad4d7e
Hala Ibrahim <halaibrahim867@gmail.com>
2022-08-11 21:34:41 +0200

    end assignment 6 of week 6

---
+++ Week6/ass6.php
@@ -0,0 +1,8 @@
+<?php
+    $money = ["Ahmed" => 100, "Sayed" => 150, "Osama" => 100, "Maher" => 250];
+
+    foreach($money as $name => $salary){
+        echo "The Name Is $name And I Need $salary Pound From Him " . "<br>";
+
+    }
+?>
