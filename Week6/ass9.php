067ce3735581c1435bbbea118e31e8765aa6247a
Hala Ibrahim <halaibrahim867@gmail.com>
2022-08-11 10:04:16 +0200

    end assignment 9 of week 6

---
+++ Week6/ass9.php
@@ -0,0 +1,10 @@
+<?php
+    $help_num = 3;
+    $nums = [4, 5, 6, 1, 2, 3];
+    $names = ["Ahmed", "Sayed", "Osama", "Mahmoud", "Gamal"];
+
+    for($i= 1; $i< $help_num ;$i++){
+        echo $names[$i ]. "<br>";
+    }
+
+?>
